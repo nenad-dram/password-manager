ee0524537c3ef36437ef3a754edd598265d8037c
nenad.dramicanin <nenaddramicanin@gmail.com>
2020-04-26 15:52:31 +0200

    Added basic error logging, added requirements file, updated .gitignore (ignore pyinstaller files)

--- password_manager/view/window_util.py
+++ password_manager/view/window_util.py
@@ -2,7 +2,7 @@ import platform
 import os
 from model import util
 from tkinter import messagebox
-
+import logging
 
 ICON_DIR = 'resources'
 ICON_NAME = 'padlock'
@@ -29,8 +29,7 @@ def get_center_points(root, width, height):
     return center_x, center_y
 
 
-def handle_app_exception(exctype, value, tb):
-    print('Type:', exctype)
-    print('Value:', value)
-    print('Traceback:', tb)
+def handle_app_exception(exc_type, exc_value, exc_traceback):
+    logging.basicConfig(filename='error.log', level=logging.DEBUG, format="%(asctime)s:%(message)s")
+    logging.error("ERROR:", exc_info=(exc_type, exc_value, exc_traceback))
     messagebox.showerror('Error', 'An error occurred during action execution!')
